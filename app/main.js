cdcb18141ab94034063ac547269dc66abce25e44
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2020-06-17 18:16:16 +0200

    Finished working on buttons from top; all buttons and their functionality finished

--- app/main.js
+++ app/main.js
@@ -1,16 +1,75 @@
 const { app, BrowserWindow, dialog } = require('electron');
 const fs = require('fs');
+
 var mainWindow = null;
 
-const openFile = file => {
+const openFile = (exports.openFile = file => {
+  console.log(file)
   fs.readFile(file, (err, data) => {
+    console.log(data.toString())
     if (err) {
       console.log(err)
       return;
     }
 
+    app.addRecentDocument(file);
     mainWindow.webContents.send('file-opened', file, data.toString());
   })
+});
+
+exports.saveHTML = (htmlContent) => {
+  file = dialog.showSaveDialog(mainWindow, {
+    title: 'Save your fu...ng HTML file!',
+    defaultPath: app.getPath('desktop'),
+    filters: [
+      {
+        name: 'HTML files',
+        extensions: ['html']
+      }
+    ]
+  });
+
+  if (!file) return;
+
+  fs.writeFile(file, htmlContent, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  })
+
+  openFile(file);
+  return file;
+};
+
+exports.saveFile = (file, content) => {
+  if (!file) {
+    file = dialog.showSaveDialog(mainWindow, {
+      title: 'Save your fu...ng file!',
+      defaultPath: app.getPath('desktop'),
+      filters: [
+        {
+          name: 'Text files',
+          extensions: ['txt']
+        },
+        {
+          name: 'Markdown files',
+          extensions: ['md', 'mdown', 'markdown']
+        }
+      ]
+    });
+  }
+
+  if (!file) return;
+
+  fs.writeFile(file, content, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  })
+  openFile(file);
+  return file;
 };
 
 exports.getFile = () => {
@@ -25,6 +84,10 @@ exports.getFile = () => {
       {
         name: 'Text Files',
         extensions: ['txt', 'text']
+      },
+      {
+        name: 'HTML Files',
+        extensions: ['html', 'htm']
       }
     ]
   });
@@ -41,6 +104,5 @@ app.on('ready', () => {
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
-
   })
-})
+});
--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,8 @@
 const marked = require('marked');
-const { remote, ipcRenderer } = require('electron')
+const { remote, ipcRenderer, shell } = require('electron')
+const path = require('path');
+
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -11,25 +14,145 @@ const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
+let originalContent = '';
+let filePath = null;
+const mainFile = remote.require('./main');
+const acceptableFormats = ['text/plain', 'text/html', 'text/markdown'];
+
+const updateUserInterface = isEdited => {
+  let title = 'Fire Sale';
+
+  if (filePath)
+    title = `${path.basename(filePath)} - ${title}`;
+
+  if (isEdited) {
+    title += " (Edited)"
+  }
+
+  currentWindow.setTitle(title);
+
+  if (filePath) currentWindow.setRepresentedFilename(filePath)
+  currentWindow.setDocumentEdited(isEdited)
+};
+
 const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const configureDisableForRevertButton = (isDisabled) => {
+  revertButton.disabled = isDisabled;
+};
+
+const configureDisableForSaveButton = (isDisabled) => {
+  saveMarkdownButton.disabled = isDisabled;
+};
+
+const configureDisableForHTMLButton = (isDisabled) => {
+  saveHtmlButton.disabled = isDisabled;
+};
+
+const isHtmlFile = (file) => {
+  return file.indexOf('.html') !== -1
+};
+
+const isRightFormatOfFile = (file, formats) => {
+  return formats.includes(file.type)
+}
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+  updateUserInterface(currentContent != originalContent);
+  configureDisableForRevertButton(currentContent == originalContent);
+  configureDisableForSaveButton(currentContent == originalContent);
+  configureDisableForHTMLButton(currentContent == originalContent)
+
+  if (filePath && isHtmlFile(filePath)) {
+    configureDisableForHTMLButton(currentContent == originalContent)
+  }
 });
 
+saveHtmlButton.addEventListener('click', () => {
+  const fileName = mainFile.saveHTML(htmlView.innerHTML);
+
+  if (fileName) {
+    filePath = fileName;
+    originalContent = markdownView.value;
+    configureDisableForHTMLButton(true)
+  }
+});
+
+showFileButton.addEventListener('click', () => {
+  if (!filePath) {
+    alert("Niste izabrali fajl")
+    return;
+  }
+
+  shell.showItemInFolder(filePath)
+})
+
 openFileButton.addEventListener('click', () => {
-  const mainFile = remote.require('./main');
-  console.log(mainFile);
   mainFile.getFile();
 })
 
+openInDefaultButton.addEventListener('click', () => {
+  shell.openItem(filePath)
+})
+
+saveMarkdownButton.addEventListener('click', evt => {
+  const fileName = mainFile.saveFile(filePath, markdownView.value);
+  if (fileName) {
+    filePath = fileName;
+    originalContent = markdownView.value;
+    configureDisableForRevertButton(true)
+    configureDisableForSaveButton(true)
+  }
+});
+
 ipcRenderer.on('file-opened', (evt, file, content) => {
-  // console.log(evt)
-  // console.log(file)
-  // console.log(content)
+  filePath = file;
+  // if (!isHtmlFile(filePath)) {
+  //   originalContent = content;
+  //   markdownView.value = content;
+  //   renderMarkdownToHtml(content);
+  // }
+  originalContent = content;
   markdownView.value = content;
   renderMarkdownToHtml(content);
+  updateUserInterface(false); //isEdited je uvek true kada se otvori fajl
+
+  showFileButton.disabled = !filePath;
+  openInDefaultButton.disabled = !filePath;
+});
+
+document.addEventListener('drop', evt => evt.preventDefault())
+document.addEventListener('dragover', evt => evt.preventDefault())
+document.addEventListener('dragstart', evt => evt.preventDefault())
+document.addEventListener('dragleave', evt => evt.preventDefault())
+
+markdownView.addEventListener('dragover', evt => {
+  const draggedFiles = evt.dataTransfer.items[0];
+
+  if (isRightFormatOfFile(draggedFiles.length > 0 ? draggedFiles[0] : draggedFiles, acceptableFormats)) {
+    markdownView.classList.add('drag-over')
+  } else {
+    markdownView.classList.add('drag-error')
+  }
+})
+
+markdownView.addEventListener('dragleave', evt => {
+  markdownView.classList.remove('drag-over')
+  markdownView.classList.remove('drag-error')
+})
+
+markdownView.addEventListener('drop', evt => {
+  const droppedFiles = evt.dataTransfer.files[0];
+  markdownView.classList.remove('drag-over')
+  markdownView.classList.remove('drag-error')
+
+  if (isRightFormatOfFile(droppedFiles.length > 0 ? droppedFiles[0] : droppedFiles, acceptableFormats)) {
+    console.log(droppedFiles);
+    mainFile.openFile(droppedFiles.path)
+  }
 })
